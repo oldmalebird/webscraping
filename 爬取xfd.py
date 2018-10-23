bfca83b4f4de08ae8dc976cd03bc2da9090cc8b5
oldmalebird <lgnlgnlgnlgn@gmail.com>
2018-10-23 17:27:50 +0800

    爬取xfd菜价
    间隔1秒会在2000+秒后拒绝请求，间隔2秒应该没问题。
    价格中出现千分位符则无法转为float，在写入数据库前加个判断即可。

---
+++ 爬取xfd.py
@@ -0,0 +1,75 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient()  # mongodb server
+xfd_veg_price = client.xfd_db.xfd_veg_price  # xfd_veg_price collection
+
+# PRICE_DATA = []
+headers = {
+    'Accept':
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding':
+    'gzip, deflate',
+    'Accept-Language':
+    'zh-CN,zh;q=0.9',
+    'Cache-Control':
+    'max-age=0',
+    'Connection':
+    'keep-alive',
+    'Cookie':
+    'UM_distinctid=166658c87bc270-05130348356c7d-2711938-2a3000-166658c87bd3c0; CNZZDATA5463821=cnzz_eid%3D1414737999-1539300290-http%253A%252F%252Fwww.xinfadi.com.cn%252F%26ntime%3D1540263448; Hs2u_2132_lastvisit=1540264789',
+    'Host':
+    'www.xinfadi.com.cn',
+    'Referer':
+    'http://www.xinfadi.com.cn/marketanalysis/1/list/1.shtml',
+    'Upgrade-Insecure-Requests':
+    '1',
+    'user-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+}
+
+
+def get_price(url):
+    '''获取xfd蔬菜价格数据'''
+    result = requests.get(url, headers=headers)
+    soup = BeautifulSoup(result.content,
+                         'lxml')  #soup= BeautifulSoup(req.content, 'lxml')
+    pricetable = soup.find('table', attrs={'class': 'hq_table'})
+    trs = pricetable.find_all('tr')[1:]
+    for tr in trs:
+        tds = tr.find_all('td')
+        veg_name = list(tds[0].stripped_strings)[0]
+        low_price = list(tds[1].stripped_strings)[0]
+        avg_price = list(tds[2].stripped_strings)[0]
+        high_price = list(tds[3].stripped_strings)[0]
+        specification = list(tds[4].stripped_strings)[0]
+        unit = list(tds[5].stripped_strings)[0]
+        date = list(tds[6].stripped_strings)[0]
+        if (',' not in low_price) and (',' not in avg_price) and (
+                ',' not in high_price):
+            data = {
+                'veg_name': veg_name,
+                'low_price': float(low_price),
+                'avg_price': float(avg_price),
+                'high_price': float(high_price),
+                'specification': specification,
+                'unit': unit,
+                'date': date
+            }
+            price_id = xfd_veg_price.insert(data)  #insert db
+
+
+if __name__ == '__main__':
+    # 列表生成器生成需要遍历的url
+    urls = [
+        'http://www.xinfadi.com.cn/marketanalysis/1/list/{}.shtml'.format(
+            str(i)) for i in range(3903, 3904)
+    ]
+    i = 2074
+    for url in urls:
+        get_price(url)
+        print('已爬取第%d页' % i)
+        i += 1
+        time.sleep(2)
