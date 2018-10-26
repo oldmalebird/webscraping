20e63655ba6b49044e1e8517119f8efd49cfe566
oldmalebird <lgnlgnlgnlgn@gmail.com>
2018-10-26 17:28:11 +0800

    synch

---
+++ test.py
@@ -0,0 +1,9 @@
+a = "/uff08/u4e00/uff09/u4e3b/u8981/u852c/u83dc/u5730/u5934/u6279/u53d1/u4ef7/u683c/u65ec/u5ea6/u8c03/u67e5/u8868"
+b = "/uff08/u4e00/uff09/u4e3b/u8981/u852c/u83dc/u5730/u5934/u6279/u53d1/u4ef7/u683c/u65ec/u5ea6/u8c03/u67e5/u8868"
+
+a = "http://202.127.42.179/scweb/WebReport/TaskGroupOutputUnit.aspx?FolderID=1&RegionID=999999&TaskID=60&ServerID=1&TaskDate=20180621&QuesID=1-519&Title=%uff08%u4e00%uff09%u4e3b%u8981%u852c%u83dc%u5730%u5934%u6279%u53d1%u4ef7%u683c%u65ec%u5ea6%u8c03%u67e5%u8868&SelectDate=2018-6-21&RePage=TaskManager1.aspx&GradeId=3"
+
+b = "http://202.127.42.179/scweb/WebReport/TaskGroupOutputUnit.aspx?FolderID=1&RegionID=999999&TaskID=60&ServerID=1&TaskDate=20180621&QuesID=1-519&Title=%uff08%u4e00%uff09%u4e3b%u8981%u852c%u83dc%u5730%u5934%u6279%u53d1%u4ef7%u683c%u65ec%u5ea6%u8c03%u67e5%u8868&SelectDate=2018-6-21&RePage=TaskManager1.aspx&GradeId=3"
+print(a == b)
+print(a.encode('utf-8'))
+#print(isinstance(a, 'unicode'))
